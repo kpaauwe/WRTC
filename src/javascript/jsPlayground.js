bfda19852958dc1d12447cb291442a7e26c46df1
kpaauwe <kpaauwe04@gmail.com>
2025-03-02 12:11:22 -0800

    printPrime function implemented

--- src/javascript/jsPlayground.js
+++ src/javascript/jsPlayground.js
@@ -1,14 +1,22 @@
 function printPrime() {
-    var n= prompt("Find prime numbers up to: ");
-    primeNumbers = [];
-    primeNumbers.push(2);
-    primeNumbers.push(3);
-    primeNumbers.push(5);
-    primeNumbers.push(7);
-    for (let i =2; i <= n;  i++) {
-        if (!(i%2==0 || i%3==0 || i%5==0 || i%7==0)) {
-            primeNumbers.push(i);
+    var n = 0;
+    while (n < 1) {
+        n = prompt("Find prime numbers up to: ");
+    }
+    const primeNumbers = [];
+    var num = 2;
+    while (num < n) {
+        let isPrime = true;
+        for (let i =2; i <= Math.sqrt(num);  i++) {
+            if (num % i === 0) {
+                isPrime = false; 
+                break;
+            }
         }
+        if (isPrime) {
+            primeNumbers.push(num);
+        }
+        num++;
     }
     const iterator = primeNumbers.values();
     for (let value of iterator) {
